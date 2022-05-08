f324b513a9451b89eb787d8f20c88f2f4fddd8e1
GTL <GTL@DESKTOP-0LQB8T5>
2022-05-07 21:40:34 -0300

    Commit de inicio del tp

---
+++ TP5_Grupo_38/src/main/java/fi/unju/edu/ar/tp5/Tp5Grupo38Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.tp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5Grupo38Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5Grupo38Application.class, args);
+	}
+
+}
---
+++ TP5_Grupo_38/src/test/java/fi/unju/edu/ar/tp5/Tp5Grupo38ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.tp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5Grupo38ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
